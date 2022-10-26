e5a1dffabe77c7d8e4153663d4cf0a951a08eb4a
hvicil <100441731+hvicil@users.noreply.github.com>
2022-10-26 23:41:11 +0200

    26102340

--- src/pages/users/home-page.js
+++ src/pages/users/home-page.js
@@ -2,7 +2,7 @@ import React from "react";
 import HomeSearch from "../../components/users/home-search";
 
 const HomePage = () => {
-  return <div>Denem denem deneme console.log("Deneme");</div>;
+  return <HomeSearch />;
 };
 
 export default HomePage;
